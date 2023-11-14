64d0ca5388720b8fed5f586ff44d4faeba101c01
Arthur Felipe Marques <arthur_f_marques@estudante.sesisenai.org.br>
2023-11-13 21:55:59 -0300

    adicionado globo

--- script.js
+++ script.js
@@ -5,19 +5,29 @@ const altitude = document.getElementById("altitude");
 const velocidade = document.getElementById("velocidade");
 var primeiraVez = true;
 
-var map = L.map('mapId').setView([0, 0], 15);
 
-var issIcon = L.icon({
+function initialize() {
+    var earth = new WE.map('mapId');
+    WE.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
+        attribution: '© OpenStreetMap contributors'
+    }).addTo(earth);
+}
+
+
+
+/*var map = L.map('mapId').setView([0, 0], 15);*/
+
+/*var issIcon = L.icon({
     iconUrl: 'ISS.png',
     iconSize: [90,50],
     iconAnchor: [25, 16]
-})
+})*/
 
-var marker = L.marker([0,0], {icon: issIcon}).addTo(map);
+/*var marker = L.marker([0,0], {icon: issIcon}).addTo(map);*/
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+/*L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+}).addTo(map);*/
 
 const url = "https://api.wheretheiss.at/v1/satellites/25544";
 
