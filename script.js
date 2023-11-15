72f6340458bbbe146cd8c25ef34f22e4d056ff59
Arthur Felipe Marques <zzanshin@gmail.com>
2023-11-15 14:42:12 -0300

    adicionado funcionalidade ao switch de unidades e foi padronizado o zoom

--- script.js
+++ script.js
@@ -3,31 +3,13 @@ const latitude = document.getElementById("latitude");
 const longitude = document.getElementById("longitude");
 const altitude = document.getElementById("altitude");
 const velocidade = document.getElementById("velocidade");
+const switchUnidade = document.getElementById("switchUnidade");
 var primeiraVez = true;
-
-
 
 var earth = new WE.map('mapId');
 WE.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
     attribution: '© OpenStreetMap contributors'
 }).addTo(earth);
-
-
-
-
-/*var map = L.map('mapId').setView([0, 0], 15);*/
-
-/*var issIcon = L.icon({
-    iconUrl: 'ISS.png',
-    iconSize: [90,50],
-    iconAnchor: [25, 16]
-})*/
-
-/*var marker = L.marker([0,0], {icon: issIcon}).addTo(map);*/
-
-/*L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);*/
 
 const url = "https://api.wheretheiss.at/v1/satellites/25544";
 
@@ -39,21 +21,42 @@ function buscarDados() {
 
         latitude.textContent = dados.latitude.toFixed(5);
         longitude.textContent = dados.longitude.toFixed(5);
-        altitude.textContent = parseInt(dados.altitude);
-        velocidade.textContent = parseInt(dados.velocity);
+        if (switchUnidade.checked) {
+            altitude.textContent = Math.round(dados.altitude / 3.281)  + "ft";
+            velocidade.textContent = Math.round(dados.velocity / 1.609)  + " mph";
+        } else {
+            altitude.textContent = Math.round(dados.altitude) + "m";
+            velocidade.textContent = Math.round(dados.velocity) + " km/h";
+        }
+        
 
-        /*marker.setLatLng([dados.latitude, dados.longitude]);*/
+        if (primeiraVez) {
+            
+            /* se for a primeira vez que a função foi chamada, definir o zoom para 2.1 nas coordenadas [0, 0]
+            para padronizar o zoom (pois quanto mais extrema for a latitude, mais sensível é o zoom, levando a 
+            inconsistências) */
 
-        if (primeiraVez) { // se for a primeira vez que a função foi chamada, definir o zoom para 2
-            earth.setView([dados.latitude, dados.longitude], 2);
+            earth.setView([0,0], 2.1);
             primeiraVez = false;
 
-            return;
         }
         
         earth.setView([dados.latitude, dados.longitude]);
 
     })
+}
+
+function alternarUnidade() {
+
+    if (switchUnidade.checked) {
+        
+        altitude.textContent = Math.round(parseInt(altitude.textContent) / 3.281) + "ft";
+        velocidade.textContent = Math.round(parseInt(velocidade.textContent) / 1.609) + " mph";
+    } else {
+        altitude.textContent = Math.round(parseInt(altitude.textContent) * 3.281) + "m";
+        velocidade.textContent = Math.round(parseInt(velocidade.textContent) * 1.609) + " km/h";
+    }
+
 }
 
 buscarDados();
